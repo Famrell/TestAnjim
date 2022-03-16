0b83974a2ccf50eb5b9d453625618b49b2931956
Venz17 <sunxyz87@gmail.com>
2022-03-16 14:35:34 +0700

    test

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9',
+}
+
+// Other
+global.owner = ['6281368971807']
+global.pemilik = ['6281368971807']
+global.premium = ['6281368971807']
+global.pengguna = 'Vlynn'
+global.botnma = 'Lynn Bot'
+global.footer = '© Lynnn' //FooterText Biar Di Bawah Nya Ada Text 
+global.web = 'https://youtube.com/c/Venzz - BOT' // Ubah Sama Lu Web Nya Bebas 
+global.web2 = 'https://github.com/Venz17' //Ubah Jga Bebas Sama lu 
+global.ganti = 'Youtube Ch' //Edit Terserah Lu Mau Apa
+global.ganti2 = 'Github'
+global.ownernma = 'Vlynn'
+global.packname = 'Lynn Bot'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/Lycho.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
